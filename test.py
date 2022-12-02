bc2325e8fb9441df321635f5caec0a8b2bc2341b
solidlime_raus <solidlime@gmail.com>
2022-12-02 21:34:34 +0900

    tetris

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("Hello World")
+# test.py
+
+import turtle
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# ゲーム画面を初期化する
+turtle.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# ゲーム画面を描画する
+turtle.title("Tetris")
+turtle.bgcolor("black")
+turtle.tracer(0)
+
+# ゲームを実行する
+while True:
+    turtle.update()
